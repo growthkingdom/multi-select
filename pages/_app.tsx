f446e2ff9108e5157a14166ad2a6fc762db49515
Cesar Fonseca <cesar@growthkingdom.com>
2022-08-16 20:23:20 -0500

    WIP

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.scss';
+import type { AppProps } from 'next/app';
+// import dynamic from 'next/dynamic';
+
+// const DynamicComponentWithNoSSR = dynamic((): any => import('../public/script'), { ssr: false });
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
